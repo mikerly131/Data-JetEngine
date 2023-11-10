e9dc0af37b41b4e05bcc71d52242091d09d4b61d
Mike <mike@ZipcodersMBP83.fios-router.home>
2023-11-10 13:14:27 -0500

    ch 15 several modifications made

--- mt.py
+++ mt.py
@@ -1,6 +1,19 @@
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+
+input_index = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
+plt.style.use('Solarize_Light2')
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.scatter(2, 4, s=200)
+# ax.plot(input_index, squares, linewidth=3)
+
+# Set chart title and label the axes
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels
+ax.tick_params(axis='both', which='major', labelsize=14)
+
 plt.show()

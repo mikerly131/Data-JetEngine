d5877cb45b16ba368b17e99f8f884a99200b831b
Mike <mike@ZipcodersMBP83.fios-router.home>
2023-11-10 15:02:26 -0500

    ch15 and a cube plot

--- cube_plot.py
+++ cube_plot.py
@@ -1,20 +1,20 @@
 from matplotlib import pyplot as plt
 
 x_index = range(1, 1001)
-y_index = [x**2 for x in x_index]
+y_index = [x**3 for x in x_index]
 
 plt.style.use('Solarize_Light2')
 fig, ax = plt.subplots()
-ax.scatter(x_index, y_index, c=y_index, cmap=plt.cm.Blues, s=10)
+ax.scatter(x_index, y_index, c=y_index, cmap=plt.cm.Reds, s=5)
 # ax.plot(input_index, squares, linewidth=3)
 
 # Set chart title and label the axes
-ax.set_title("Square Numbers", fontsize=24)
+ax.set_title("Cube Numbers", fontsize=20)
 ax.set_xlabel("Value", fontsize=14)
-ax.set_ylabel("Square of Value", fontsize=14)
+ax.set_ylabel("Cube of Value", fontsize=14)
 
 # Set the range for each axis
-ax.axis([0, 1100, 0, 1100000])
+ax.axis([0, 1100, 0, 1100000000])
 
 # Set size of tick labels
 # ax.tick_params(axis='both', which='major', labelsize=14)

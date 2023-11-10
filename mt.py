fc28e530cd027d5396a4ced6a7c791c6b822b15f
Mike <mike@ZipcodersMBP83.fios-router.home>
2023-11-10 13:30:18 -0500

    more progress through ch15, changing plot colors

--- mt.py
+++ mt.py
@@ -1,17 +1,20 @@
 from matplotlib import pyplot as plt
 
-input_index = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
+x_index = range(1, 1001)
+y_index = [x**2 for x in x_index]
 
 plt.style.use('Solarize_Light2')
 fig, ax = plt.subplots()
-ax.scatter(2, 4, s=200)
+ax.scatter(x_index, y_index, c=(0.8, 0.2, 0.2), s=10)
 # ax.plot(input_index, squares, linewidth=3)
 
 # Set chart title and label the axes
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
+
+# Set the range for each axis
+ax.axis([0, 1100, 0, 1100000])
 
 # Set size of tick labels
 ax.tick_params(axis='both', which='major', labelsize=14)
